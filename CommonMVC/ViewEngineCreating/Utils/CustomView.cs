eceeacb4c134615cf21603524aff06ad956a75e9
Anatoly Shandov <anatolyshandov@gmail.com>
2019-06-03 23:29:36 +0300

    ViewEngineCreating_p1

---
+++ CommonMVC/ViewEngineCreating/Utils/CustomView.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewEngineCreating.Utils
+{
+    public class CustomView : IView
+    {
+        string Path { get; set; }
+        public CustomView(string viewPath)
+        {
+            Path = viewPath;
+        }
+
+        public async Task RenderAsync(ViewContext context)
+        {
+            string content = String.Empty;
+            using(FileStream viewStream=new FileStream(Path, FileMode.Open))
+            {
+                using(StreamReader sr=new StreamReader(viewStream))
+                {
+                    content = sr.ReadToEnd(); 
+                }
+            }
+             await context.Writer.WriteAsync(content);
+        }
+    }
+}
---
+++ CommonMVC/ViewEngineCreating/Utils/CustomViewEngine.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewEngineCreating.Utils
+{
+    public class CustomViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindView(ActionContext context, string viewName, bool isMainPage)
+        {
+            return ViewEngineResult.NotFound(viewName, new string[] { });
+        }
+
+        public ViewEngineResult GetView(string executingFilePath, string viewPath, bool isMainPage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
