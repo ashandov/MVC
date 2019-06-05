b7370589d8790af12b93e45874060fda0a0af4b0
ashandov <45596607+ashandov@users.noreply.github.com>
2019-06-05 16:22:23 +0300

    Update HomeController.cs

--- CommonMVC/ViewEngineCreating/Controllers/HomeController.cs
+++ CommonMVC/ViewEngineCreating/Controllers/HomeController.cs
@@ -1,26 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace ViewEngineCreating.Controllers
 {
+    [Route("Store")]
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Route(" ")]
+        [Route("homepage")]
         public IActionResult Index()
         {
-            var controller=RouteData.Values["controller"].ToString();
-            var action = RouteData.Values["action"].ToString();
-            return Content($"controller: {controller}  action:{action}");
-        }
-        public IActionResult About()
-        {
             return View();
         }
+        [Route("{id:int}/{name:maxlength(10)}")]
+        [Route("Test")]
+        public IActionResult Test(int id,string name)
+        {
+            return Content($"id: {id}  name:{name}");
+        }
+        [Route("{name}")]
+        public IActionResult About(string name)
+        {
+            return Content($"name:{name}");
+        }
+       [Route("[controller]/[action]/{id?}")]
         public IActionResult Contact()
         {
-            return View();
+            var controller = RouteData.Values["controller"].ToString();
+            var action = RouteData.Values["action"].ToString();
+
+            return Content($"controller :{controller} | action: {action}");
         }
     }
 }
